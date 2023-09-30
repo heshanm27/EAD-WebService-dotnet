e22b29ebc09951aa2b4da7cc616a2f5f421daf33
HeshanMadhuranga <39455525+heshanm27@users.noreply.github.com>
2023-10-01 01:48:56 +0530

    User Api Route Update

--- WebApi/Controllers/TrainController.cs
+++ WebApi/Controllers/TrainController.cs
@@ -41,13 +41,81 @@ namespace EAD_WebService.Controllers
             return Ok(response);
         }
 
-
-        //
-        [HttpPatch("{id}/tickets")]
+        [HttpPatch("{id}/ticket")]
         public async Task<ActionResult<ServiceResponse<EmptyData>>> addTicket(string id, List<Tickets> trainIn)
         {
 
             ServiceResponse<EmptyData> response = await _trainService.addTickets(id, trainIn);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
+
+        }
+
+        [HttpPatch("{id}/ticket/{ticketId}")]
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> updateTicket(string id, string ticketId, Tickets trainIn)
+        {
+
+            ServiceResponse<EmptyData> response = await _trainService.updateTickets(id, ticketId, trainIn);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
+
+        }
+
+        [HttpDelete("{id}/ticket/{ticketId}")]
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> removeTicket(string id, string ticketId)
+        {
+
+            ServiceResponse<EmptyData> response = await _trainService.removeTickets(id, ticketId);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
+
+        }
+
+
+        [HttpPatch("{id}/activate")]
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> updateActive(string id)
+        {
+
+            ServiceResponse<EmptyData> response = await _trainService.activateTrainSchedule(id);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
+
+        }
+
+
+        [HttpPatch("{id}/deactivate")]
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> updateDeactive(string id)
+        {
+
+            ServiceResponse<EmptyData> response = await _trainService.deactivateTrainSchedule(id);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
+
+        }
+
+
+        [HttpPatch("{id}/publish")]
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> updatePublish(string id)
+        {
+
+            ServiceResponse<EmptyData> response = await _trainService.publishTrainSchedule(id);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
+
+        }
+
+
+        [HttpPatch("{id}/unpublish")]
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> updateUnpublish(string id)
+        {
+
+            ServiceResponse<EmptyData> response = await _trainService.unpublishTrainSchedule(id);
 
             if (!response.Status) return BadRequest(response);
             return Ok(response);
--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -7,30 +7,76 @@ namespace EAD_WebService.Controllers
     public class UserController : ControllerBase
     {
 
-        [HttpGet]
-        public ActionResult<List<User>> Get()
+
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
         {
-            return Ok();
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get([FromQuery] BasicFilters filters)
+        {
+            ServiceResponse<List<User>> response = await _userService.getUsers(filters);
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<User> Get(string id)
+        public async Task<ActionResult<User>> Get(string id)
         {
-            return Ok();
+            ServiceResponse<User> response = await _userService.getUser(id);
+
+            if (!response.Status) return BadRequest(response);
+
+            return Ok(response);
         }
 
         [HttpPatch("{id}")]
 
-        public IActionResult Put(string id, User userIn)
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> Put(string id, User userIn)
         {
-            return Ok();
+            ServiceResponse<EmptyData> response = await _userService.updateUser(id, userIn);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
         }
 
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(string id)
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> Delete(string id)
         {
-            return Ok();
+            ServiceResponse<EmptyData> response = await _userService.removeUser(id);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
+        }
+
+        [HttpPatch("{id}/activate")]
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> activateUser(string id)
+        {
+            ServiceResponse<EmptyData> response = await _userService.activateUser(id);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
+        }
+
+        [HttpPatch("{id}/deactivate")]
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> deactivateUser(string id)
+        {
+            ServiceResponse<EmptyData> response = await _userService.deativateUser(id);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
+        }
+
+        [HttpPatch("{id}/role")]
+        public async Task<ActionResult<ServiceResponse<EmptyData>>> deactivateUser(string id, UserEnum userEnum)
+        {
+            ServiceResponse<EmptyData> response = await _userService.updateUserRole(id, userEnum);
+
+            if (!response.Status) return BadRequest(response);
+            return Ok(response);
         }
 
     }
