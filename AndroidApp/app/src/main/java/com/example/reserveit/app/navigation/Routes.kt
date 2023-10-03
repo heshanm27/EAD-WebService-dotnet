d76e34fda8789846cc9eca241950319f73d6b16b
HeshanMadhuranga <39455525+heshanm27@users.noreply.github.com>
2023-10-03 22:44:49 +0530

    android project update

--- AndroidApp/app/src/main/java/com/example/reserveit/app/navigation/Routes.kt
+++
@@ -1,9 +0,0 @@
-package com.example.reserveit.app.navigation
-
-object Routes {
-    const val LOGIN_SCREEN = "login";
-    const val REGISTER_SCREEN = "register";
-    const val HOME_SCREEN = "home";
-    const val PROFILE_SCREEN = "profile";
-    const val RESERVATION_SCREEN = "reservation";
-}
